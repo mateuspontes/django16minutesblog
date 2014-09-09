98b2bf36196e7233e4913e17de6716a20d54aaac
Mateus <mateuspo10@gmail.com>
2014-09-09 00:42:39 -0300

    Initial

---
+++ blog/blog/settings.py
@@ -0,0 +1,51 @@
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+
+DEBUG = True
+TEMPLATE_DEBUG = True
+ALLOWED_HOSTS = []
+SECRET_KEY = '1*aox%m0*p)mir3xek$)g%zg84+k2#xois5wat-kj)y)kp_2#w'
+
+INSTALLED_APPS = (
+  'django.contrib.admin',
+  'django.contrib.auth',
+  'django.contrib.contenttypes',
+  'django.contrib.sessions',
+  'django.contrib.messages',
+  'django.contrib.staticfiles',
+)
+
+MIDDLEWARE_CLASSES = (
+  'django.contrib.sessions.middleware.SessionMiddleware',
+  'django.middleware.common.CommonMiddleware',
+  'django.middleware.csrf.CsrfViewMiddleware',
+  'django.contrib.auth.middleware.AuthenticationMiddleware',
+  'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+  'django.contrib.messages.middleware.MessageMiddleware',
+  'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'blog.urls'
+WSGI_APPLICATION = 'blog.wsgi.application'
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+  'default': {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+  }
+}
+
+LANGUAGE_CODE = 'pt-br'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+STATIC_URL = os.path.join(BASE_DIR, '/static/')
+TEMPLATE_URL = os.path.join(BASE_DIR, '/templates/')
